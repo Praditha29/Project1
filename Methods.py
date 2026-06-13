6d91a6761657c8a1af852e3c9a805e825c40669c
Praditha29 <pradithareddy@gmail.com>
2026-06-13 16:03:57 +0530

    Added security

--- Methods.py
+++ Methods.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
 #Temporary in-memory database
 todos = []
 
@@ -52,3 +59,4 @@ def delete_todo(todo_id: int):
             todos.pop()
             return{"message": "Data deleted"}
     return{"Error": "Todo not found"}
+
